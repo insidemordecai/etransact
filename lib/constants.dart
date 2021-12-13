949a5f218bc1bc34f3712afce7269ef2580223a0
itsmordecai <mordecai.kipngetich@gmail.com>
2021-12-13 11:49:14 +0300

    new: constants file

---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+RoundedRectangleBorder kRoundedBorder = RoundedRectangleBorder(
+  borderRadius: BorderRadius.circular(18.0),
+  // side: BorderSide(color: Colors.red)
+);
