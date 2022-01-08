9428054a4011061a1a5407efaa143edfbd3c20ed
itsmordecai <mordecai.kipngetich@gmail.com>
2022-01-08 12:16:21 +0300

    update: rollback

--- lib/home.dart
+++ lib/home.dart
@@ -1,14 +1,20 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
-import 'package:firebase_storage/firebase_storage.dart';
-import 'package:path/path.dart' as path;
+import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:pdf_viewer_jk/pdf_viewer_jk.dart';
 
 import 'landing_page.dart';
 
-class Home extends StatelessWidget {
+class Home extends StatefulWidget {
   Home({this.uid});
   final String uid;
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
   final String title = "Home";
 
   @override
@@ -34,100 +40,15 @@ class Home extends StatelessWidget {
           )
         ],
       ),
-      body: HomePageBody(),
-      drawer: NavigateDrawer(uid: this.uid),
-    );
-  }
-}
-
-class HomePageBody extends StatefulWidget {
-  @override
-  _HomePageBodyState createState() => _HomePageBodyState();
-}
-
-class _HomePageBodyState extends State<HomePageBody> {
-  FirebaseStorage storage = FirebaseStorage.instance;
-
-  Future<List<Map<String, dynamic>>> _loadPDFs() async {
-    List<Map<String, dynamic>> files = [];
-
-    final ListResult result = await storage.ref().list();
-    final List<Reference> allFiles = result.items;
-
-    await Future.forEach<Reference>(allFiles, (file) async {
-      final String fileUrl = await file.getDownloadURL();
-      final FullMetadata fileMeta = await file.getMetadata();
-      files.add({
-        "url": fileUrl,
-        "path": file.fullPath,
-        "uploaded_by": fileMeta.customMetadata?['uploaded_by'] ?? 'Nobody',
-        "description":
-            fileMeta.customMetadata?['description'] ?? 'No description'
-      });
-    });
-
-    return files;
-  }
-
-  // Delete selected PDF
-  Future<void> _delete(String ref) async {
-    await storage.ref(ref).delete();
-    // Rebuild the UI
-    setState(() {});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            Text('Welcome!'),
-            Expanded(
-              child: FutureBuilder(
-                future: _loadPDFs(),
-                builder: (context,
-                    AsyncSnapshot<List<Map<String, dynamic>>> snapshot) {
-                  if (snapshot.connectionState == ConnectionState.done) {
-                    return ListView.builder(
-                      itemCount: snapshot.data?.length ?? 0,
-                      itemBuilder: (context, index) {
-                        final Map<String, dynamic> image = snapshot.data![index];
-
-                        return Card(
-                          margin: EdgeInsets.symmetric(vertical: 10),
-                          child: ListTile(
-                            dense: false,
-                            leading: Image.network(image['url']),
-                            title: Text(image['uploaded_by']),
-                            subtitle: Text(image['description']),
-                            trailing: IconButton(
-                              onPressed: () => _delete(image['path']),
-                              icon: Icon(
-                                Icons.delete,
-                                color: Colors.red.shade300,
-                              ),
-                            ),
-                          ),
-                        );
-                      },
-                    );
-                  }
-
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
+      body: Center(
+        child: Text('Welcome!'),
       ),
+      drawer: NavigateDrawer(uid: this.widget.uid),
     );
   }
 }
 
+// Sidebar Drawer
 class NavigateDrawer extends StatefulWidget {
   final String uid;
   NavigateDrawer({Key key, this.uid}) : super(key: key);
@@ -202,3 +123,4 @@ class _NavigateDrawerState extends State<NavigateDrawer> {
     );
   }
 }
+// End of Sidebar Drawer
