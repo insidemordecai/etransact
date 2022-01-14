2cfbe013e6e4cc4cd0cadeff0b512773491e8dbf
itsmordecai <mordecai.kipngetich@gmail.com>
2022-01-14 11:36:30 +0300

    update: remade buttons from scratch and removed button dependency

--- lib/screens/landing_page.dart
+++ lib/screens/landing_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_signin_button/flutter_signin_button.dart';
 
 import 'email_login.dart';
 import 'email_signup.dart';
@@ -7,6 +6,7 @@ import 'package:e_transaction/constants.dart';
 
 class LandingPage extends StatelessWidget {
   final String title = "eTransact";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -19,38 +19,42 @@ class LandingPage extends StatelessWidget {
             children: <Widget>[
               Padding(
                 padding: EdgeInsets.all(10.0),
-                child: Text("eTransact",
+                child: Text(title,
                     style: TextStyle(
                         fontWeight: FontWeight.bold,
                         fontSize: 50,
                         fontFamily: 'Roboto')),
               ),
               Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: SignInButton(
-                    Buttons.Email,
-                    text: "Create Account",
+                padding: EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
                     shape: kRoundedBorder,
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => EmailSignUp()),
-                      );
-                    },
-                  )),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EmailSignUp()),
+                    );
+                  },
+                  child: Text('Create Account'),
+                ),
+              ),
               Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: SignInButton(
-                    Buttons.Email,
-                    text: "Sign In",
+                padding: EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
                     shape: kRoundedBorder,
-                    onPressed: () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(builder: (context) => EmailLogIn()),
-                      );
-                    },
-                  )),
+                  ),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => EmailLogIn()),
+                    );
+                  },
+                  child: Text('Sign In'),
+                ),
+              ),
             ]),
       ),
     );
