79c8fafe2aa6b3057ba61edc5931ffc0e2a2338a
itsmordecai <mordecai.kipngetich@gmail.com>
2022-01-17 16:21:06 +0300

    update: redone login page and fix: button not reappearing after wrong creds

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,8 +1,24 @@
+import 'package:e_transaction/model/palette.dart';
 import 'package:flutter/material.dart';
 
 RoundedRectangleBorder kRoundedBorder = RoundedRectangleBorder(
   borderRadius: BorderRadius.circular(18.0),
-  // side: BorderSide(color: Colors.red)
 );
 
-const kFixedSize = const Size(200, 30); //for the buttons 
+const kFixedSize = const Size(200, 30); //for the buttons
+
+const kTextFieldDecoration = InputDecoration(
+  hintText: '',
+  contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+  border: OutlineInputBorder(
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  enabledBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Palette.kTeal, width: 1.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+  focusedBorder: OutlineInputBorder(
+    borderSide: BorderSide(color: Palette.kTeal, width: 2.0),
+    borderRadius: BorderRadius.all(Radius.circular(32.0)),
+  ),
+);
---
+++ lib/screens/login.dart
@@ -0,0 +1,121 @@
+//largely based on https://github.com/itsmordecai/flash-chat/blob/master/lib/screens/login_screen.dart
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+import 'home.dart';
+import 'package:e_transaction/constants.dart';
+
+class LogIn extends StatefulWidget {
+  static const String id = 'login';
+
+  @override
+  _LogInState createState() => _LogInState();
+}
+
+class _LogInState extends State<LogIn> {
+  final _auth = FirebaseAuth.instance;
+  bool showSpinner = false;
+
+  late String email;
+  late String password;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Log In")),
+      body: ModalProgressHUD(
+        inAsyncCall: showSpinner,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 24.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextField(
+                keyboardType: TextInputType.emailAddress,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your email',
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              TextField(
+                obscureText: true,
+                textAlign: TextAlign.center,
+                onChanged: (value) {
+                  password = value;
+                },
+                decoration: kTextFieldDecoration.copyWith(
+                  hintText: 'Enter your password',
+                ),
+              ),
+              SizedBox(
+                height: 24.0,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: kRoundedBorder,
+                  fixedSize: kFixedSize,
+                ),
+                onPressed: () async {
+                  setState(() {
+                    showSpinner = true;
+                  });
+                  try {
+                    await _auth.signInWithEmailAndPassword(
+                      email: email,
+                      password: password,
+                    );
+                    Navigator.pushNamed(context, Home.id);
+                    setState(() {
+                      showSpinner = false;
+                    });
+                  } on FirebaseAuthException catch (e) {
+                    if (e.code == 'user-not-found') {
+                      print('No user found for that email');
+                      setState(() {
+                        showSpinner = false;
+                      });
+
+                      Fluttertoast.showToast(
+                        msg:
+                            'No user found for that email. Re-enter credentials',
+                      );
+                    } else if (e.code == 'wrong-password') {
+                      print('Wrong password provided for that user');
+                      setState(() {
+                        showSpinner = false;
+                      });
+
+                      Fluttertoast.showToast(
+                        msg: 'Wrong password. Re-enter credentials',
+                      );
+                    } else {
+                      print(e);
+                      setState(() {
+                        showSpinner = false;
+                      });
+
+                      Fluttertoast.showToast(
+                        msg: 'Error. Try again!',
+                      );
+                    }
+                  }
+                },
+                child: Text('Log In'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
