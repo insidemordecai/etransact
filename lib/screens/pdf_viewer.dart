ea7e32d4185006c74b84fe408ee6cde1fc608d09
Mordecai K <mordecai.kipngetich@gmail.com>
2022-05-25 12:26:31 +0300

    remove unnecessary code

--- lib/screens/pdf_viewer.dart
+++ lib/screens/pdf_viewer.dart
@@ -50,10 +50,7 @@ class _PDFViewerPageState extends State<PDFViewerPage> {
       ),
       body: PDFView(
         filePath: widget.file.path,
-        // autoSpacing: false,
         // swipeHorizontal: true,
-        // pageSnap: false,
-        // pageFling: false,
         onRender: (pages) => setState(() => this.pages = pages!),
         onViewCreated: (controller) =>
             setState(() => this.controller = controller),
